13fcabbea9bb48fd9b3b49abec82a14a76c52eab
Victor S. Borges <vborges@gmail.com>
2020-07-16 16:09:30 -0300

    Uses images as sections

--- src/SectionThree/SectionThree.jsx
+++ src/SectionThree/SectionThree.jsx
@@ -1,32 +1,18 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faQuoteLeft, faQuoteRight } from '@fortawesome/free-solid-svg-icons'
 
-import politicianPhoto from '../politician-photo.png';
+import sectionThreeImg from '../image-section-green.png';
 import './SectionThree.css';
 
 function SectionThree({ description }) {
   return (
     <div className="section-three">
-      <div className="text-box">
-        <Row>
-          <Col md="auto" className="quote-left">
-            <FontAwesomeIcon icon={faQuoteLeft} size="2x" />{' '}
-          </Col>
-          <Col>
-            Cresci aqui nesta cidade maravilhosa, terra acolhedora e de gente que luta, batalha e
-            corre atrás buscando novas conquistas. Acredito que podemos fazer sim, um governo
-            participativo, construindo novas ideias para nossa Senhor do Bonfim.
-          </Col>
-          <Col md="auto" className="quote-right">
-            <FontAwesomeIcon icon={faQuoteRight} size="2x" />{' '}
-          </Col>
-        </Row>
-      </div>
-      <div className="title-box">
-        <img src={politicianPhoto} alt={description} />
-      </div>
+      <img
+        src={sectionThreeImg}
+        alt={`Cresci aqui nesta cidade maravilhosa, terra acolhedora e de gente que luta,
+              batalha e corre atrás buscando novas conquistas. Acredito que podemos fazer
+              sim, um governo participativo, construindo novas ideias para nossa Senhor do
+              Bonfim. - Laércio Jr.`}
+      />
     </div>
   );
 }
--- src/SectionTwo/SectionTwo.jsx
+++ src/SectionTwo/SectionTwo.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
+import sectionTwoImage from '../image-section-blue.png';
 import './SectionTwo.css';
 
 function SectionTwo() {
   return (
     <div className="section-two">
-      <div className="left-box">
-        O QUE É O PROGRAMA PARTICIPATIVO?
-      </div>
-      <div className="right-box">
-        O Programa de Governo Participativo é um projeto voltado a população para que juntos
-        possamos apontar, discutir e opinar sobre as políticas públicas para a nossa cidade.
-        <br /><br />
-        Acredito que podemos defender e construir uma nova Senhor do Bonfim com a opinião de todos.
-        <br /><br />
-        PARTICIPE!
-      </div>
+      <img
+        alt={`O Programa de Governo Participativo é um projeto voltado a população para que juntos
+              possamos apontar, discutir e opinar sobre as políticas públicas para a nossa cidade.`}
+        src={sectionTwoImage}
+      />
     </div>
   );
 }
